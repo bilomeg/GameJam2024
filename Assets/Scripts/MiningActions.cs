95374160b987ec0b5492034bb8b404264007966d
EmmyFortin <fortin.emmy@gmail.com>
2024-01-10 23:37:12 -0500

    bug

--- Assets/Scripts/MiningActions.cs
+++ Assets/Scripts/MiningActions.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class MiningActions : MonoBehaviour
 {
    RaycastHit hit;
+   RaycastHit hit2;
    [SerializeField]GameObject finder;
+    public LayerMask layerMask;
+   
     private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-        if (Physics.Raycast (ray, out hit, 100)) {
+        if(Input.GetMouseButtonDown(0)){
+             Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+               int layerMask = 1 << LayerMask.NameToLayer("Player");
+              layerMask = ~layerMask;
+
+
+        if (Physics.Raycast (ray, out hit, 100,layerMask)) {
             Debug.Log (hit.transform.name);
             Debug.Log ("hit");
             finder.transform.LookAt(hit.transform.position);
+            Ray ray2 = new Ray(finder.transform.position, hit.transform.position-finder.transform.position);
+
+            
+            if(Physics.Raycast (ray2, out hit2, 100)){
+                hit2.transform.GetComponent<Renderer>().material.color = Color.red;
+                //Destroy(hit2.transform.gameObject);
+                
+            }
+            
+        }
+       
             
 
         }
