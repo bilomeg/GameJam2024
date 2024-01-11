53c3b197c5cc83d04f6096d7e3f1c503f0d7f201
EmmyFortin <fortin.emmy@gmail.com>
2024-01-11 08:29:28 -0500

    points scripts

--- Assets/Scripts/ScriptablesObjectsScene1/InfosDataMining.cs
+++ Assets/Scripts/ScriptablesObjectsScene1/InfosDataMining.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName="InfosDataMining", menuName ="SO/InfosNouveauMining")]
 public class InfosDataMining : ScriptableObject
 {
