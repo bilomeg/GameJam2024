7c1dd760b65062ad89e5f7859efa9b267963f74e
Amelie Cousineau <ameliecousineau5@gmail.com>
2024-01-11 01:41:52 -0500

    Ajout du mouvement des projectiles

--- Assets/Scripts/SpaceInvader/DeplacementJoueur.cs
+++ Assets/Scripts/SpaceInvader/DeplacementJoueur.cs
@@ -5,8 +5,14 @@ using UnityEngine.InputSystem;
 
 public class DeplacementJoueur : MonoBehaviour
 {
+    //Pour le déplacement du joueur
     [SerializeField] private float vitesse = 3.0f;
     private float mouvementHorizontal;
+
+    //Pour le shoot
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float vitesseProjectile = 3.0f;
+     private List<GameObject> projectilesEnMouvement = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,7 @@ public class DeplacementJoueur : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BougeProjectiles();
         Bouge();
     }
 
@@ -27,8 +34,30 @@ public class DeplacementJoueur : MonoBehaviour
         mouvementHorizontal = touchesClavier.x * -1;
     }
 
+    public void OnShoot(InputValue value)
+    {
+        GameObject nouveauProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+        projectilesEnMouvement.Add(nouveauProjectile);
+    }
+
     void Bouge(){
         transform.Translate(Vector3.forward * Time.deltaTime * vitesse * mouvementHorizontal);
-        Debug.Log(mouvementHorizontal);
+    }
+
+    void BougeProjectiles()
+    {
+        for (int i = 0; i < projectilesEnMouvement.Count; i++)
+        {
+            if (projectilesEnMouvement[i] != null)
+            {
+                projectilesEnMouvement[i].transform.Translate(Vector3.up * Time.deltaTime * vitesseProjectile);
+            }
+            else
+            {
+                // Retirer le projectile s'il a été détruit
+                projectilesEnMouvement.RemoveAt(i);
+                i--;
+            }
+        }
     }
 }
