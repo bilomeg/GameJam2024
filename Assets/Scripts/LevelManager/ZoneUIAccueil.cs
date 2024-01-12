25fffbfa0458bbc1967c78b4e4c99d55a993ede0
bilomeg <megane@bilocreations.com>
2024-01-12 10:00:08 -0500

    sdf

--- Assets/Scripts/LevelManager/ZoneUIAccueil.cs
+++ Assets/Scripts/LevelManager/ZoneUIAccueil.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using System.Threading.Tasks;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class ZoneUIAccueil : MonoBehaviour
 {
-    
- 
+    [SerializeField] private AudioMixer audioMixer;
     private LevelManager _levelManager;
        // Start is called before the first frame update
     void Start()
@@ -21,6 +21,25 @@ public class ZoneUIAccueil : MonoBehaviour
         _levelManager.LoadAsyncScene("SceneSallePrincipale");
       
     }
+
+    public void MasterVolumeChange(Slider slider)
+    {
+        float value = slider.value * 80;
+        audioMixer.SetFloat("MasterVolume", value -80);
+    }
+
+    public void MusicVolumeChange(Slider slider)
+    {
+        float value = slider.value * 80;
+        audioMixer.SetFloat("MusicVolume", value -80);
+    }
+
+    public void SFXVolumeChange(Slider slider)
+    {
+        float value = slider.value * 80;
+        audioMixer.SetFloat("SFXVolume", value -80);
+    }
+
    public void QuiteGame()
     {
         Debug.Log($"fonctionne");
