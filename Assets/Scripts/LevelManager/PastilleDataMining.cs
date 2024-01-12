3ee4a52deec0c322662869551d1d943ed6ed3802
Pierre-LucGagne <pierreluc.gagne10@gmail.com>
2024-01-11 20:06:06 -0500

    Ajout du player

--- Assets/Scripts/LevelManager/PastilleDataMining.cs
+++ Assets/Scripts/LevelManager/PastilleDataMining.cs
@@ -7,6 +7,7 @@ public class PastilleDataMining : MonoBehaviour
     private LevelManager _levelManager;
 
     // Start is called before the first frame update
+    [SerializeField] private GameObject _canvasToucheE;
     void Start()
     {
         _levelManager = LevelManager.Instance;
@@ -15,6 +16,11 @@ public class PastilleDataMining : MonoBehaviour
     // Update is called once per frame
    private void OnTriggerEnter(Collider other)
  {
+    //_levelManager.LoadAsyncScene("SceneDataMining");
+    _canvasToucheE.SetActive(true);
+ }
+
+ private void OnOpen( ){
     _levelManager.LoadAsyncScene("SceneDataMining");
  }
 }
