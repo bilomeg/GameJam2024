08ba018bafd2ae84a8a1eb336c5fe27a360e889f
Amelie Cousineau <ameliecousineau5@gmail.com>
2024-01-12 06:51:18 -0500

    page d'accueil

---
+++ Assets/Scripts/ChangementSceneAccueil.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangementSceneAccueil : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangementScene", 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangementScene(){
+        LevelManager.Instance.LoadAsyncScene("SceneIntro");
+    }
+}
